8ce4145cf0e731a5e2e8a688693c71e737375dc8
Markus Ressel <mail@markusressel.de>
2019-04-02 01:33:51 +0200

    formatting

--- tests/test_config.py
+++ tests/test_config.py
@@ -13,23 +13,30 @@ class ConfigTests(N26TestBase):
     PATH = os.path.dirname(os.path.abspath(__file__))
     CONFIG_FILE = os.path.join(PATH, "test_creds.yml")
 
-    EXPECTED_FILE_CONF = config.Config(username='john.doe@example.com',
-                                       password='$upersecret')
+    EXPECTED_FILE_CONF = config.Config(
+        username='john.doe@example.com',
+        password='$upersecret')
 
-    EXPECTED_ENV_CONF = config.Config(username='john.doe@env.com',
-                                      password='env!?')
+    EXPECTED_ENV_CONF = config.Config(
+        username='john.doe@env.com',
+        password='env!?')
 
     def test_preconditions(self):
         assert os.path.exists(self.CONFIG_FILE)
 
-    @mock.patch.dict(os.environ,
-                     {ENV_PARAM_USER: EXPECTED_ENV_CONF.username, ENV_PARAM_PASSWORD: EXPECTED_ENV_CONF.password})
+    @mock.patch.dict(os.environ, {
+        ENV_PARAM_USER: EXPECTED_ENV_CONF.username,
+        ENV_PARAM_PASSWORD: EXPECTED_ENV_CONF.password
+    })
     def test_environment_variable(self):
         result = config.get_config()
         self.assertEqual(result, self.EXPECTED_ENV_CONF)
 
     # ensure environment is empty
-    @mock.patch.dict(os.environ, {ENV_PARAM_USER: "", ENV_PARAM_PASSWORD: ""})
+    @mock.patch.dict(os.environ, {
+        ENV_PARAM_USER: "",
+        ENV_PARAM_PASSWORD: ""
+    })
     def test_file(self):
         # use test file path instead of real one
         config.CONFIG_FILE_PATH = self.CONFIG_FILE
