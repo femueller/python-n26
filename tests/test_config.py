3fd44e9178c212c4f809a6b3e2ad034dbf19f145
Markus Ressel <mail@markusressel.de>
2019-04-01 23:54:52 +0200

    config test fixes

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,11 +6,11 @@ from n26 import config
 from n26.config import ENV_PARAM_USER, ENV_PARAM_PASSWORD
 from tests.test_api_base import N26TestBase
 
-PATH = os.path.dirname(os.path.abspath(__file__))
-test_creds = os.path.join(PATH, "test_creds.yml")
-
 
 class ConfigTests(N26TestBase):
+    PATH = os.path.dirname(os.path.abspath(__file__))
+    CONFIG_FILE = os.path.join(PATH, "test_creds.yml")
+
     EXPECTED_FILE_CONF = config.Config(username='john.doe@file.com',
                                        password='file!?')
 
@@ -18,7 +18,7 @@ class ConfigTests(N26TestBase):
                                       password='env!?')
 
     def test_preconditions(self):
-        assert os.path.exists(test_creds)
+        assert os.path.exists(self.CONFIG_FILE)
 
     @mock.patch.dict(os.environ,
                      {ENV_PARAM_USER: EXPECTED_ENV_CONF.username, ENV_PARAM_PASSWORD: EXPECTED_ENV_CONF.password})
@@ -29,12 +29,8 @@ class ConfigTests(N26TestBase):
     # ensure environment is empty
     @mock.patch.dict(os.environ, {ENV_PARAM_USER: "", ENV_PARAM_PASSWORD: ""})
     def test_file(self):
-        # patch path to local test file
-        def mockreturn(path):
-            return test_creds
-
-        # TODO: what is this?
-        # monkeypatch.setattr(os.path, "expanduser", mockreturn)
+        # use test file path instead of real one
+        config.CONFIG_FILE_PATH = self.CONFIG_FILE
 
         result = config.get_config()
         self.assertEqual(result, self.EXPECTED_FILE_CONF)
