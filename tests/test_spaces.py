c0bbeb9b6082311ae0302b09a4f1703b75e4934a
Markus Ressel <mail@markusressel.de>
2019-04-02 01:04:07 +0200

    added spaces

---
+++ tests/test_spaces.py
@@ -0,0 +1,11 @@
+from n26.api import GET
+from tests.test_api_base import N26TestBase, mock_api
+
+
+class SpacesTests(N26TestBase):
+    """Spaces tests"""
+
+    @mock_api(method=GET, response_file="spaces.json")
+    def test_get_spaces(self):
+        result = self._underTest.get_spaces()
+        self.assertIsNotNone(result)
